569f2c4f862693ad66203ff501a7525319cd3016
NNrepos <nivimano@gmail.com>
2021-09-25 18:12:17 +0300

    fixed weird merge

--- pi_code/tflite/shoot_birds.py
+++ pi_code/tflite/shoot_birds.py
@@ -1,18 +1,11 @@
 import argparse
 import os
 import time
-from datetime import datetime
 from threading import Thread
-from typing import List
 
 import cv2
 import numpy as np
-from firebase_admin import messaging
-
-try:
-    from tflite_runtime.interpreter import Interpreter
-except ImportError:
-    from tensorflow.lite.python.interpreter import Interpreter
+from tflite_runtime.interpreter import Interpreter
 
 MAX_FPS = 30
 BIRD_CONFIDENCE = 0.7
@@ -22,7 +15,7 @@ BIRD_LABEL = "bird"
 class VideoStream:
     """Camera object that controls video streaming from the Picamera"""
 
-    def __init__(self, resolution=(640, 480)):
+    def __init__(self, resolution=(640, 480), framerate=30):
         # Initialize the PiCamera and the camera image stream
         self.stream = cv2.VideoCapture(0)
         self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
@@ -38,7 +31,6 @@ class VideoStream:
     def start(self):
         # Start the thread that reads frames from the video stream
         Thread(target=self.update, args=()).start()
-        time.sleep(1)
         return self
 
     def update(self):
@@ -62,176 +54,152 @@ class VideoStream:
         self.stopped = True
 
 
-class BigScaryOwl:
-    def __init__(self):
-        args = self.get_input_arguments()
-        model_name = args.modeldir
-        graph_name = args.graph
-        labelmap_name = args.labels
-        cwd_path = os.getcwd()
+# Define and parse input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--modeldir', help='Folder the .tflite file is located in',
+                    required=True)
+parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite',
+                    default='detect.tflite')
+parser.add_argument('--labels', help='Name of the labelmap file, if different than labelmap.txt',
+                    default='labelmap.txt')
+parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
+                    default=0.5)
+parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
+                    default='1280x720')
 
-        self.min_confidence_threshold = float(args.threshold)
-        self.im_width, self.im_height = [int(val) for val in args.resolution.split('x')]
+args = parser.parse_args()
 
-        # Path to .tflite file, and .txt file, which contain the model network and labels
-        self.path_to_model = os.path.join(cwd_path, model_name, graph_name)
-        self.path_to_labels = os.path.join(cwd_path, model_name, labelmap_name)
+MODEL_NAME = args.modeldir
+GRAPH_NAME = args.graph
+LABELMAP_NAME = args.labels
+min_conf_threshold = float(args.threshold)
+resW, resH = args.resolution.split('x')
+imW, imH = int(resW), int(resH)
 
-        self.labels = self.parse_labels()
+CWD_PATH = os.getcwd()
 
-        # Load the Tensorflow Lite model.
-        self.interpreter = Interpreter(model_path=self.path_to_model)
-        self.interpreter.allocate_tensors()
+# Path to .tflite file, which contains the model that is used for object detection
+PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
 
-        # Get model details
-        self.network_input = self.interpreter.get_input_details()
-        self.network_output = self.interpreter.get_output_details()
-        self.height = self.network_input[0]['shape'][1]
-        self.width = self.network_input[0]['shape'][2]
+# Path to label map file
+PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
 
-        self.floating_model = (self.network_input[0]['dtype'] == np.float32)
+# Load the label map
+with open(PATH_TO_LABELS, 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
 
-        self.input_mean = 127.5
-        self.input_std = 127.5
+# Have to do a weird fix for label map if using the COCO "starter model" from
+# https://www.tensorflow.org/lite/models/object_detection/overview
+# First label is '???', which has to be removed.
+if labels[0] == '???':
+    del (labels[0])
 
-        # Initialize frame rate calculation
-        self.frame_rate_calc = 0.0
-        self.freq = cv2.getTickFrequency()
+# Load the Tensorflow Lite model.
+interpreter = Interpreter(model_path=PATH_TO_CKPT)
+interpreter.allocate_tensors()
 
-        # Initialize video stream
-        self.videostream = VideoStream(resolution=(self.im_width, self.im_height)).start()
+# Get model details
+network_input = interpreter.get_input_details()
+network_output = interpreter.get_output_details()
+height = network_input[0]['shape'][1]
+width = network_input[0]['shape'][2]
 
-        # firebase stuff
-        # TODO@niv
-        self.firebase_registration_token = 'YOUR_REGISTRATION_TOKEN'
+floating_model = (network_input[0]['dtype'] == np.float32)
 
-        # TODO@niv: create TF network class
+input_mean = 127.5
+input_std = 127.5
 
-    def parse_labels(self) -> List[str]:
-        # Load the label map
-        with open(self.path_to_labels, 'r') as f:
-            labels = [line.strip() for line in f.readlines()]
-        # Have to do a weird fix for label map if using the COCO "starter model" from
-        # https://www.tensorflow.org/lite/models/object_detection/overview
-        # First label is '???', which has to be removed.
-        return labels[1:]
 
-    @staticmethod
-    def get_input_arguments():
-        # Define and parse input arguments
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-m', '--modeldir', help='Folder the .tflite file is located in',
-                            required=True)
-        parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite',
-                            default='detect.tflite')
-        parser.add_argument('--labels', help='Name of the labelmap file, if different than labelmap.txt',
-                            default='labelmap.txt')
-        parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
-                            default=0.5)
-        parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
-                            default='1280x720')
-        return parser.parse_args()
+def main():
+    # Initialize frame rate calculation
+    frame_rate_calc = 1
+    freq = cv2.getTickFrequency()
 
-    def run_video_loop(self):
-        print("press q (while focused on video) to quit")
-        while True:
-            # Start timer (for calculating frame rate)
-            t1 = cv2.getTickCount()
+    # Initialize video stream
+    videostream = VideoStream(resolution=(imW, imH), framerate=MAX_FPS).start()
+    time.sleep(1)
+    print("press q (while focused on video) to quit")
+    while True:
+        # Start timer (for calculating frame rate)
+        t1 = cv2.getTickCount()
 
-            frame, input_data = self.prepare_frame_for_network(self.videostream)
-
-            self.run_image_through_network(input_data)
-            self.analyze_detections(frame)
-
-            # Draw framerate in corner of frame
-            cv2.putText(frame, 'FPS: {0:.2f}'.format(self.frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
-
-            # All the results have been drawn on the frame, so it's time to display it.
-            cv2.imshow('Object detector', frame)
-
-            # Calculate framerate
-            t2 = cv2.getTickCount()
-            time1 = (t2 - t1) / self.freq
-            self.frame_rate_calc = 1 / time1
-
-            # Press 'q' to quit
-            if cv2.waitKey(1) == ord('q'):
-                break
-
-        # Clean up
-        cv2.destroyAllWindows()
-        self.videostream.stop()
-
-    def prepare_frame_for_network(self, videostream):
         # Grab frame from video stream
-        frame_from_cam = videostream.read()
+        frame1 = videostream.read()
+
         # Acquire frame and resize to expected shape [1xHxWx3]
-        frame = frame_from_cam.copy()
+        frame = frame1.copy()
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_resized = cv2.resize(frame_rgb, (self.width, self.height))
+        frame_resized = cv2.resize(frame_rgb, (width, height))
         input_data = np.expand_dims(frame_resized, axis=0)
+
         # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-        if self.floating_model:
-            input_data = (np.float32(input_data) - self.input_mean) / self.input_std
-        return frame, input_data
+        if floating_model:
+            input_data = (np.float32(input_data) - input_mean) / input_std
 
-    def run_image_through_network(self, input_data):
-        """
-        note: tensorflow_lite is optimized for ARM! super slow on windows.
-        """
-        # Perform the actual detection by running the model with the image as input
-        self.interpreter.set_tensor(self.network_input[0]['index'], input_data)
-        self.interpreter.invoke()
+        run_image_through_network(input_data)
+        draw_detections(frame)
 
-    def analyze_detections(self, frame):
-        # Retrieve detection results
-        boxes = self.interpreter.get_tensor(self.network_output[0]['index'])[0]  # Bounding box coordinates of detected objects
-        classes = self.interpreter.get_tensor(self.network_output[1]['index'])[0]  # Class index of detected objects
-        scores = self.interpreter.get_tensor(self.network_output[2]['index'])[0]  # Confidence of detected objects
-        # Loop over all detections and draw detection box if confidence is above minimum threshold
-        for detection_id in range(len(scores)):
-            curr_confidence = scores[detection_id]
-            curr_label = self.labels[int(classes[detection_id])]
+        # Draw framerate in corner of frame
+        cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
 
-            if (curr_confidence > self.min_confidence_threshold) and (curr_confidence <= 1.0):
-                self.draw_detection(boxes, curr_label, frame, detection_id, scores)
+        # All the results have been drawn on the frame, so it's time to display it.
+        cv2.imshow('Object detector', frame)
 
-            if curr_confidence > BIRD_CONFIDENCE and curr_label == BIRD_LABEL:
-                print(f"bird found with confidence {curr_confidence}")
-                self.send_data_firebase(z, z)
+        # Calculate framerate
+        t2 = cv2.getTickCount()
+        time1 = (t2 - t1) / freq
+        frame_rate_calc = 1 / time1
+
+        # Press 'q' to quit
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    # Clean up
+    cv2.destroyAllWindows()
+    videostream.stop()
 
 
-    def draw_detection(self, boxes, object_name, frame, i, scores):
-        # Get bounding box coordinates and draw box
-        # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-        ymin = int(max(1, (boxes[i][0] * self.im_height)))
-        xmin = int(max(1, (boxes[i][1] * self.im_width)))
-        ymax = int(min(self.im_height, (boxes[i][2] * self.im_height)))
-        xmax = int(min(self.im_width, (boxes[i][3] * self.im_width)))
-        cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
-
-        # Example: 'person: 72%'
-        label = '%s: %d%%' % (object_name, int(scores[i] * 100))
-        # Get font size
-        label_size, base_line = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
-        # Make sure not to draw label too close to top of window
-        label_ymin = max(ymin, label_size[1] + 10)
-        # Draw white box to put label text in
-        cv2.rectangle(frame, (xmin, label_ymin - label_size[1] - 10), (xmin + label_size[0], label_ymin + base_line - 10), (255, 255, 255), cv2.FILLED)
-        cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)  # Draw label text
-
-    def send_data_firebase(self, label, confidence):
-        message = messaging.Message(
-            data={
-                'label': label,
-                'confidence':confidence,
-                'time': datetime.now(),
-            },
-            token=self.firebase_registration_token)
-
-        response = messaging.send(message)
-        print('Successfully sent message:', response)
+def run_image_through_network(input_data):
+    """
+    note: tensorflow_lite is optimized for ARM! super slow on windows.
+    """
+    # Perform the actual detection by running the model with the image as input
+    interpreter.set_tensor(network_input[0]['index'], input_data)
+    interpreter.invoke()
 
 
-if __name__ == "__main__":
-    BigScaryOwl().run_video_loop()
+def draw_detections(frame):
+    # Retrieve detection results
+    boxes = interpreter.get_tensor(network_output[0]['index'])[0]  # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(network_output[1]['index'])[0]  # Class index of detected objects
+    scores = interpreter.get_tensor(network_output[2]['index'])[0]  # Confidence of detected objects
+    # Loop over all detections and draw detection box if confidence is above minimum threshold
+    for i in range(len(scores)):
+        if (scores[i] > min_conf_threshold) and (scores[i] <= 1.0):
+            if scores[i] > BIRD_CONFIDENCE and labels[int(classes[i])] == BIRD_LABEL:
+                print(f"bird found with confidence {scores[i]}")
+
+            # Get bounding box coordinates and draw box
+            # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+            ymin = int(max(1, (boxes[i][0] * imH)))
+            xmin = int(max(1, (boxes[i][1] * imW)))
+            ymax = int(min(imH, (boxes[i][2] * imH)))
+            xmax = int(min(imW, (boxes[i][3] * imW)))
+
+            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
+
+            # Draw label
+            # Look up object name from "labels" array using class index
+            object_name = labels[int(classes[i])]
+            # Example: 'person: 72%'
+            label = '%s: %d%%' % (object_name, int(scores[i] * 100))
+            # Get font size
+            label_size, base_line = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+            # Make sure not to draw label too close to top of window
+            label_ymin = max(ymin, label_size[1] + 10)
+            # Draw white box to put label text in
+            cv2.rectangle(frame, (xmin, label_ymin - label_size[1] - 10), (xmin + label_size[0], label_ymin + base_line - 10), (255, 255, 255), cv2.FILLED)
+            cv2.putText(frame, label, (xmin, label_ymin - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)  # Draw label text
+
+
+main()
