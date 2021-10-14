9c489b60c4b6a0ac99dd0ff3ad2acaef8865a091
NNrepos <nivimano@gmail.com>
2021-10-14 12:14:50 +0300

    refactored network and sounds, added sound, requirements.txt

--- pi_code/tflite/shoot_birds.py
+++ pi_code/tflite/shoot_birds.py
@@ -1,26 +1,22 @@
 import argparse
-import os
+import random
 import time
 from pathlib import Path
 from threading import Thread
-from typing import List, Union
+from typing import List
 
 import cv2
 import firebase_admin
 import numpy as np
+import pygame
 from firebase_admin import credentials, db
-from playsound import playsound
-
-FIREBASE_KEY_JSON = "firebase_key.json"
+from pygame import mixer, event
 
 try:
+    print("setting up tensorflow, this takes ~10 seconds...")
     from tflite_runtime.interpreter import Interpreter
 except ImportError:
     from tensorflow.lite.python.interpreter import Interpreter
-
-MAX_FPS = 30
-BIRD_CONFIDENCE = 0.5
-BIRD_LABEL = "bird"
 
 
 class VideoStream:
@@ -73,35 +69,29 @@ class SoundLibrary:
         self.owl_call = self.SOUNDS_PATH / "owl_call.mp3"
         self.owl_hoot = self.SOUNDS_PATH / "owl_hoot.mp3"
         self.owl_screech = self.SOUNDS_PATH / "owl_screech.mp3"
+        self.owl_surprise = self.SOUNDS_PATH / "surprise.mp3"
+        self.all_sounds = [self.owl_call, self.owl_hoot, self.owl_screech, self.owl_surprise]
+
+    def random_sound(self):
+        return random.choice(self.all_sounds)
 
 
-class BigScaryOwl:
-    RUN_NETWORK = False
+class BirdDetectionNetwork:
+    MODEL_DIR_NAME = "Sample_TFLite_model"
+    GRAPH_FILE_NAME = "detect.tflite"
+    LABELS_FILE_NAME = "labelmap.txt"
 
     def __init__(self):
-        args = self.get_input_arguments()
-        model_name = args.modeldir
-        graph_name = args.graph
-        labelmap_name = args.labels
-        cwd_path = os.getcwd()
-
-        self.sound_thread: Union[None, Thread] = None
-
-        self.sounds = SoundLibrary()
-        self.loop_ticks = 0
-        self.frame_count = 0
-
-        self.min_confidence_threshold = float(args.threshold)
-        self.im_width, self.im_height = [int(val) for val in args.resolution.split('x')]
+        cwd_path = Path.cwd()
 
         # Path to .tflite file, and .txt file, which contain the model network and labels
-        self.path_to_model = os.path.join(cwd_path, model_name, graph_name)
-        self.path_to_labels = os.path.join(cwd_path, model_name, labelmap_name)
+        self.path_to_model = cwd_path / self.MODEL_DIR_NAME / self.GRAPH_FILE_NAME
+        self.path_to_labels = cwd_path / self.MODEL_DIR_NAME / self.LABELS_FILE_NAME
 
         self.labels = self.parse_labels()
 
-        # Load the Tensorflow Lite model.
-        self.interpreter = Interpreter(model_path=self.path_to_model)
+        # Load the Tensorflow Lite model
+        self.interpreter = Interpreter(model_path=str(self.path_to_model))
         self.interpreter.allocate_tensors()
 
         # Get model details
@@ -115,19 +105,6 @@ class BigScaryOwl:
         self.input_mean = 127.5
         self.input_std = 127.5
 
-        # Initialize frame rate calculation
-        self.frame_rate_calc = 0.0
-        self.freq = cv2.getTickFrequency()
-
-        # Initialize video stream
-        self.videostream = VideoStream(resolution=(self.im_width, self.im_height)).start()
-
-        # initalize firebase app
-        cred = credentials.Certificate(FIREBASE_KEY_JSON)
-        app_data = {"databaseURL": "https://iot-project-f75da-default-rtdb.firebaseio.com/"}
-        firebase_admin.initialize_app(cred, app_data)
-        self.ref = db.reference("/")
-
     def parse_labels(self) -> List[str]:
         # Load the label map
         with open(self.path_to_labels, 'r') as f:
@@ -137,16 +114,78 @@ class BigScaryOwl:
         # First label is '???', which has to be removed.
         return labels[1:]
 
+    def transform_video_frame(self, frame_from_cam):
+        # Acquire frame and resize to expected shape [1xHxWx3]
+        frame = frame_from_cam.copy()
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb, (self.width, self.height))
+        input_data = np.expand_dims(frame_resized, axis=0)
+        # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+        if self.floating_model:
+            input_data = (np.float32(input_data) - self.input_mean) / self.input_std
+        return frame, input_data
+
+    def run_image_through_network(self, input_data):
+        """
+        note: tensorflow_lite is optimized for ARM! super slow on windows.
+        """
+        # Perform the actual detection by running the model with the image as input
+        self.interpreter.set_tensor(self.network_input[0]['index'], input_data)
+        self.interpreter.invoke()
+
+    def get_detection_results(self):
+        boxes = self.interpreter.get_tensor(self.network_output[0]['index'])[0]  # Bounding box coordinates of detected objects
+        classes = self.interpreter.get_tensor(self.network_output[1]['index'])[0]  # Class index of detected objects
+        scores = self.interpreter.get_tensor(self.network_output[2]['index'])[0]  # Confidence of detected objects
+        return boxes, classes, scores
+
+    def get_label(self, label_id):
+        return self.labels[int(label_id)]
+
+
+class BigScaryOwl:
+    RUN_NETWORK = False
+    BIRD_CONFIDENCE = 0.5
+    BIRD_LABEL = "bird"
+    FIREBASE_KEY_FILE_NAME = "firebase_key.json"
+    MUSIC_END_EVENT = pygame.USEREVENT + 1
+
+    def __init__(self):
+        self.bird_detection_scores: List = []
+        args = self._get_input_arguments()
+        self.network = BirdDetectionNetwork()
+
+        self.sounds = SoundLibrary()
+
+        # use pygame for the sound mixer
+        pygame.init()
+        mixer.init()
+        mixer.music.set_endevent(self.MUSIC_END_EVENT)
+        self.playing_sound = False
+
+        self.loop_ticks = 0
+        self.frame_count = 0
+
+        self.min_confidence_threshold = float(args.threshold)
+        self.im_width, self.im_height = [int(val) for val in args.resolution.split('x')]
+
+        # Initialize frame rate calculation
+        self.frame_rate_calc = 0.0
+        self.freq = cv2.getTickFrequency()
+
+        # Initialize video stream
+        self.videostream = VideoStream(resolution=(self.im_width, self.im_height)).start()
+
+        # initalize firebase app
+        cred = credentials.Certificate(self.FIREBASE_KEY_FILE_NAME)
+        app_data = {"databaseURL": "https://iot-project-f75da-default-rtdb.firebaseio.com/"}
+        firebase_admin.initialize_app(cred, app_data)
+        self.ref = db.reference("/")
+
     @staticmethod
-    def get_input_arguments():
+    def _get_input_arguments():
         # Define and parse input arguments
         parser = argparse.ArgumentParser()
-        parser.add_argument('-m', '--modeldir', help='Folder the .tflite file is located in',
-                            required=True)
-        parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite',
-                            default='detect.tflite')
-        parser.add_argument('--labels', help='Name of the labelmap file, if different than labelmap.txt',
-                            default='labelmap.txt')
         parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
                             default=0.5)
         parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
@@ -158,8 +197,9 @@ class BigScaryOwl:
         while True:
             self._update_ticks()
 
-            frame, input_data = self.prepare_frame_for_network(self.videostream)
-            self.run_image_through_network(input_data)
+            frame, input_data = self.network.transform_video_frame(self.videostream.read())
+            if self.RUN_NETWORK:
+                self.network.run_image_through_network(input_data)
             self.analyze_detections(frame)
             self.show_frame(frame)
             self.check_realtime_triggers()
@@ -169,6 +209,7 @@ class BigScaryOwl:
                 break
 
         # Clean up
+        self.kill_all_threads()
         cv2.destroyAllWindows()
         self.videostream.stop()
 
@@ -188,62 +229,56 @@ class BigScaryOwl:
         # All the results have been drawn on the frame, so it's time to display it.
         cv2.imshow('Object detector', frame)
 
-    def prepare_frame_for_network(self, videostream):
-        # Grab frame from video stream
-        frame_from_cam = videostream.read()
-        # Acquire frame and resize to expected shape [1xHxWx3]
-        frame = frame_from_cam.copy()
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_resized = cv2.resize(frame_rgb, (self.width, self.height))
-        input_data = np.expand_dims(frame_resized, axis=0)
-        # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-        if self.floating_model:
-            input_data = (np.float32(input_data) - self.input_mean) / self.input_std
-        return frame, input_data
-
-    def run_image_through_network(self, input_data):
-        """
-        note: tensorflow_lite is optimized for ARM! super slow on windows.
-        """
-        if self.RUN_NETWORK:
-            # Perform the actual detection by running the model with the image as input
-            self.interpreter.set_tensor(self.network_input[0]['index'], input_data)
-            self.interpreter.invoke()
-
     def analyze_detections(self, frame):
         if self.RUN_NETWORK:
-            # Retrieve detection results
-            boxes = self.interpreter.get_tensor(self.network_output[0]['index'])[0]  # Bounding box coordinates of detected objects
-            classes = self.interpreter.get_tensor(self.network_output[1]['index'])[0]  # Class index of detected objects
-            scores = self.interpreter.get_tensor(self.network_output[2]['index'])[0]  # Confidence of detected objects
+            boxes, classes, scores = self.network.get_detection_results()
+
             # Loop over all detections and draw detection box if confidence is above minimum threshold
+            is_saw_bird = False
+            best_bird_score = 0
             for detection_id in range(len(scores)):
                 curr_confidence = scores[detection_id]
-                curr_label = self.labels[int(classes[detection_id])]
+                curr_label = self.network.get_label(classes[detection_id])
 
                 if (curr_confidence > self.min_confidence_threshold) and (curr_confidence <= 1.0):
                     self.draw_detection(boxes, curr_label, frame, detection_id, scores)
 
-                if curr_confidence > BIRD_CONFIDENCE and curr_label == BIRD_LABEL:
-                    self._bird_detected_action()
-                    # print(f"bird found with confidence {curr_confidence}")
-                    # self.send_data_firebase(curr_label, curr_confidence)
+                if curr_label == self.BIRD_LABEL:
+                    is_saw_bird = True
+                    best_bird_score = max(best_bird_score, curr_confidence)
+                    if self._is_bird_high_certainty(curr_confidence):
+                        self._bird_detected_action()
+                        # print(f"bird found with confidence {curr_confidence}")
+                        # self.send_data_firebase(curr_label, curr_confidence)
+
+            if is_saw_bird:
+                self.bird_detection_scores.append(best_bird_score)
         else:
             if not self.frame_count % 100:
                 self._bird_detected_action()
 
-    def _bird_detected_action(self):
-        if ((self.sound_thread is None) or
-                (self.sound_thread is not None and not self.sound_thread.is_alive())):
-            self.sound_thread = Thread(target=self._play_sound_action, args=[self.sounds.owl_hoot])
-            self.sound_thread.start()
-            print(f"frames={self.frame_count}")
+    def _is_bird_high_certainty(self, curr_confidence):
+        # TODO@niv: if len(self.bird_detection_scores) > 3 and ...
+        return curr_confidence > self.BIRD_CONFIDENCE
 
-    @staticmethod
-    def _play_sound_action(sound_to_play):
-        hoot_thread = Thread(target=playsound, args=[sound_to_play])
-        hoot_thread.start()
-        hoot_thread.join()
+    def _bird_detected_action(self):
+        self._play_sound_action(self.sounds.random_sound())
+        self._flap_wings_action()
+
+        print(f"frames={self.frame_count}")
+
+    def _play_sound_action(self, sound_file_name):
+        for e in event.get():
+            if e.type == self.MUSIC_END_EVENT:
+                self.playing_sound = False
+
+        if not self.playing_sound:
+            print("starting sound_process")
+            mixer.music.load(sound_file_name)
+            mixer.music.play()
+            self.playing_sound = True
+        else:
+            print("skipping sound_process because it's still running")
 
     def draw_detection(self, boxes, object_name, frame, i, scores):
         # Get bounding box coordinates and draw box
@@ -277,6 +312,16 @@ class BigScaryOwl:
     #     print('Successfully sent message:', response)
     def check_realtime_triggers(self):
         # self.ref.set()
+        pass
+
+    def kill_all_threads(self):
+        self._stop_music()
+
+    @staticmethod
+    def _stop_music():
+        mixer.music.stop()
+
+    def _flap_wings_action(self):
         pass
 
 
