354dd8be439db4ff8c4b719c58add322fcaa6046
Ezzat Qupty <eqqupty@amazon.com>
2021-10-15 20:36:30 +0300

    Made new app in owlinator directory Realtime database is working Building app screens skeleton

--- owlinator_app/lib/data/command_dao.dart
+++ owlinator_app/lib/data/command_dao.dart
@@ -2,17 +2,15 @@ import 'package:firebase_database/firebase_database.dart';
 import 'command.dart';
 
 class CommandDao {
-
   final DatabaseReference _commandsRef =
-  FirebaseDatabase.instance.reference().child('commands');
+      FirebaseDatabase.instance.reference().child("messages");
 
   void sendCommand(Command command) {
-    _commandsRef.set(command.toJson());
     _commandsRef.push().set(command.toJson());
+    print("message saved");
   }
 
   Query getCommandQuery() {
     return _commandsRef;
   }
 }
-
---
+++ owlinator_app/lib/main.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'validator.dart';
+import 'main_page.dart';
+import 'new_page.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Owlinator',
+      theme: ThemeData(
+          primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          Builder(builder: (BuildContext context) {
+            return FlatButton(
+              child: const Text('Sign out'),
+              textColor: Theme
+                  .of(context)
+                  .buttonColor,
+              onPressed: () async {
+                final FirebaseUser user = (await _auth.currentUser()).user;
+                if (user == null) {
+                  Scaffold.of(context).showSnackBar(const SnackBar(
+                    content: Text('No one has signed in.'),
+                  ));
+                  return;
+                }
+                await _auth.signOut();
+                final String uid = user.uid;
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text(uid + ' has successfully signed out.'),
+                ));
+              },
+            );
+          })
+        ],
+      ),
+      body: Builder(builder: (BuildContext context) {
+        return ListView(
+          scrollDirection: Axis.vertical,
+          padding: const EdgeInsets.all(16),
+          children: <Widget>[
+            //TODO: UI widgets will go here.
+          ],
+        );
+      }),
+    );
+  }
+}
+
+
+class _RegisterEmailSection extends StatefulWidget {
+  final String title = 'Registration';
+  @override
+  State<StatefulWidget> createState() =>
+      _RegisterEmailSectionState();
+}
+class _RegisterEmailSectionState extends State<_RegisterEmailSection> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _success = false;
+  String _userEmail = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText:
+              'Password'),
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              alignment: Alignment.center,
+              child: RaisedButton(
+                onPressed: () async {
+                  if (_formKey.currentState.validate()) {
+                    _register();
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(_success == null
+                  ? ''
+                  : (_success
+                  ? 'Successfully registered ' + _userEmail
+                  : 'Registration failed')),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _register() async {
+    final FirebaseUser user = (await
+    _auth.createUserWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    )
+    ).user;
+    if (user != null) {
+      setState(() {
+        _success = true;
+        _userEmail = user.email;
+      });
+    } else {
+      setState(() {
+        _success = true;
+      });
+    }
+  }
+
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+}
+
