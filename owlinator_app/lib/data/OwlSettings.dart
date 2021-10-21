76d9809cd474989c1927ad193cdd4ce3ec45d2d3
Ezzat Qupty <eqqupty@amazon.com>
2021-10-21 18:48:23 +0300

    fixed bugs, changed Icons and splashscreens

---
+++ owlinator_app/lib/data/OwlSettings.dart
@@ -0,0 +1,43 @@
+import 'UserData.dart';
+
+class OwlSettings {
+  Device device;
+  bool mute;
+  bool fixedHead;
+  bool notify;
+  double angle;
+  double volume;
+  String assicatedUid;
+
+  OwlSettings(this.device, this.mute, this.fixedHead, this.notify, this.angle, this.volume, this.assicatedUid);
+
+  OwlSettings.fromJson(Map<dynamic, dynamic> json)
+      : device = (Device.fromJson(json['device'] as Map<dynamic, dynamic>)),
+        mute = json['settings']['mute'] as bool,
+        fixedHead = json['settings']['fixedHead'] as bool,
+        notify = json['settings']['notify'] as bool,
+        angle = (json['settings']['angle'] as double),
+        volume = (json['settings']['volume'] as double),
+        assicatedUid = json['settings']['assicatedUid'] as String;
+
+  OwlSettings.defaultSettings(Device device, String uid)
+      : device = device,
+        mute = false,
+        fixedHead = false,
+        notify = true,
+        angle = 0,
+        volume = 100,
+        assicatedUid = uid;
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'device': device.toJson(),
+        'settings': {
+          'mute': mute,
+          'fixedHead': fixedHead,
+          'notify': notify,
+          'angle': angle,
+          'volume': volume,
+          'assicatedUid': assicatedUid
+        }
+      };
+}
